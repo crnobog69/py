bc52b7366768debb0421af9e54598e055c98feea
crnobog <carskalavra@proton.me>
2025-03-01 13:47:46 +0100

    ❄️

--- cbrojac.rb
+++ cbrojac.rb
@@ -28,7 +28,7 @@ end
 
 puts "-" * 50
 puts " Укупна статистика:"
-puts "   Укупно (.c) датотека: #{datoteke.count}"
+puts "   Укупно (.py) датотека: #{datoteke.count}"
 puts "   Укупно карактера: #{ukupno_karaktera}"
 puts "   Укупно редова: #{ukupno_redova}"
 puts "-" * 50
