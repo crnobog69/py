b62d168deedda91f923cbde82712bce8b34cff30
crnobog <carskalavra@proton.me>
2025-03-01 11:34:07 +0100

    ❄️

---
+++ drugiSemestar-2025/test/17.py
@@ -0,0 +1,8 @@
+# 17
+
+odgovor = input("Da li bi ste hteli hranu? (D/N)")
+
+if odgovor == "D" or odgovor == "d":
+    print("Evo malo hrane!")
+else:
+    print("Nema hrane za vas.")
---
+++ drugiSemestar-2025/test/18.py
@@ -0,0 +1,8 @@
+# 18
+
+ime = input("Unesite vase ime: ")
+
+if ime == "":
+    print("Niste uneli vase ime!")
+else:
+    print(f"Zdravo, {ime}!")
