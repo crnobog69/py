8a85cc2bd5cefd3202143a04d5680f5b98929340
crnobog <carskalavra@proton.me>
2025-03-10 09:48:00 +0100

    ❄️

---
+++ drugiSemestar-2025/vezbe/p2/zadatak10.py
@@ -0,0 +1,13 @@
+# Zadatak 29
+
+import math
+
+n = int(input("Унесите n = "))
+x = int(input("Унесите x = "))
+s = 0
+p = 1
+
+for i in range(1, n + 1, 1):
+    p = p * x
+    s = s + math.cos(p)
+print(f"Сума = {s:.5f}")
---
+++ drugiSemestar-2025/vezbe/p2/zadatak11.md
@@ -0,0 +1,5 @@
+```python
+# Zadatak 30
+
+
+```
